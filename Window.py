d95ebfc7e4fff36a63628f9fd0dd30505a667430
TheOfficialFury <149468324+TheOfficialFury@users.noreply.github.com>
2023-11-22 14:20:10 +0530

    Update Window.py

--- Window.py
+++ Window.py
@@ -1,12 +1,21 @@
-import tkinter as tk
+import customtkinter
+window = ["600", "600"]
 
+app = customtkinter.CTk()
+app.geometry(window[0] + "x" + window[1])
+app.title("EQ Tester")
 
-# Defines the window element.
-window = tk.Tk()
+tabview = customtkinter.CTkTabview(master=app, width=(int(window[0]) - 20), height=(int(window[0]) - 10))
+tabview.pack(padx=0, pady=0)
 
-# Defines a "Label" element.
-label = tk.Label(text="Hey there!")
-label.pack() # This makes sure the label actually displays.
+tabview.add("Basic")  # add tab at the end
+tabview.add("Advanced")  # add tab at the end
+tabview.set("Basic")  # set currently visible tab
 
-# This line actually makes everything work. This is essential for the functioning of tkinter.
-window.mainloop() 
+def optionmenu_callback(choice):
+    print("optionmenu dropdown clicked:", choice)
+
+optionmenu = customtkinter.CTkOptionMenu(master=tabview.tab("Basic"), values=["option 1", "option 2"], command=optionmenu_callback)
+optionmenu.set("option 2")
+
+app.mainloop()
